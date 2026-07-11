8ee52b22713161400e6b6fef1106f9123aba5a13
adityatripathi10c-sketch <adityatripathi10c@gmail.com>
2026-07-11 13:31:44 +0530

    Time: 0 ms (100%), Space: 7.7 MB (59.31%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isThree(int n) {
+        int count=0;
+        for(int i=1;i*i<=n;i++){
+            if(n%i==0){
+                if((n/i)==i){
+                count++;
+
+                }
+                else{
+                    count+=2;
+                }
+            }
+        }
+        if(count==3){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
