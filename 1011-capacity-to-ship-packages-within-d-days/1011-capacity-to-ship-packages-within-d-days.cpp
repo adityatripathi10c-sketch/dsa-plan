aeff090867743441dd05feee59a208ed2cc78e33
adityatripathi10c-sketch <adityatripathi10c@gmail.com>
2026-07-28 14:38:42 +0530

    Time: 6 ms (93.28%), Space: 35 MB (88.95%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,40 +1,31 @@
 class Solution {
-public:
-    int shipWithinDays(std::vector<int>& weights, int days) {
-        // The minimum possible ship capacity must be at least the heaviest single package.
-        int left = *std::max_element(weights.begin(), weights.end());
-        // The maximum capacity needed is the sum of all packages (shipping everything in 1 day).
-        int right = std::accumulate(weights.begin(), weights.end(), 0);
-        
-        int result = right;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            if (canShipWithCapacity(weights, days, mid)) {
-                result = mid;       // 'mid' works, record it as a potential minimum
-                right = mid - 1;    // Try to find a smaller feasible capacity
-            } else {
-                left = mid + 1;     // 'mid' is too small, increase capacity
-            }
-        }
-        
-        return result;
-    }
-
 private:
-    bool canShipWithCapacity(const std::vector<int>& weights, int maxDays, int capacity) {
-        int daysNeeded = 1;
-        int currentWeight = 0;
-        
-        for (int weight : weights) {
-            if (currentWeight + weight > capacity) {
-                daysNeeded++;       // Move package to the next day
-                currentWeight = 0;  // Reset current ship load
+    int daysneeded(vector<int>& weights,int capacity){
+        int days=1;
+        int currentload=0;
+        for(int w:weights){
+            if(currentload + w>capacity){
+                days++;
+                currentload=w;
+            }else{
+                currentload+=w;
             }
-            currentWeight += weight;
+        }return days;
+    }
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int left=*max_element(weights.begin(),weights.end());
+        int right=0;
+        for(int weight:weights){
+            right+=weight;
         }
-        
-        return daysNeeded <= maxDays;
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(daysneeded(weights,mid)<=days){
+                right=mid;
+            }else{
+                left=mid+1;
+            }
+        }return left;
     }
 };
